9aea1d2471b182cb3662327f72baac96b7f65154
rerejii <190361m@ugs.kochi-tech.ac.jp>
2018-01-24 16:14:04 +0900

    カスタムコマンドserializeの追加

--- kochitabi/cms/management/commands/deserialize.py
+++ kochitabi/cms/management/commands/deserialize.py
@@ -1,11 +1,11 @@
 from django.core import serializers
 from django.core.management.base import BaseCommand
-import json, sys
+import json
 
 # BaseCommandを継承して作成
 class Command(BaseCommand):
     # python manage.py help count_entryで表示されるメッセージ
-    help = '第1引数で入力したjsonファイルを読み込みます manage.pyからみたパスを入力してください'
+    help = '第1引数で入力したjsonファイルを読み込みます manage.pyからのパスを入力してください'
 
     # コマンドライン引数を指定します。(argparseモジュール https://docs.python.org/2.7/library/argparse.html)
     # 今回はblog_idという名前で取得する。（引数は最低でも1個, int型）
